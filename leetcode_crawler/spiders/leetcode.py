b69a07ac8ee7b99857751cfdd096cdcd6a30056e
Michael Ran <michaelran16@gmail.com>
2015-12-25 06:20:39 +0800

    update crawling Xpath rule

--- leetcode_crawler/spiders/leetcode.py
+++ leetcode_crawler/spiders/leetcode.py
@@ -17,20 +17,23 @@ class LeetcodeSpider(CrawlSpider):
 
     def start_requests(self):
         URL = "https://leetcode.com/problemset/algorithms/"
+        self.logger.info('start request at: %s', URL)
         # todo: did not do login here
+
         yield Request(URL, self.parse_list)
 
     def parse_list(self, response):
+        self.logger.info('now parsing the list: %s', response.url)
         # we check problem index range in here
-        for trElement in response.xpath('//table[@id="problemList"]/tbody/tr'):
+        for trElement in response.xpath('//tbody[@class="reactable-data"]/tr'):
             # for each question, I get its ID and URL
             # ID might be checked against some predefined range, and also
-            # IDs are passed to callback as a parameter, reason being taht 
+            # IDs are passed to callback as a parameter, reason being taht
             # the actually content page does not show a ID number.
             qIndex = trElement.xpath('td[2]/text()').extract()[0]
             qUrl = trElement.xpath('td[3]/a/@href').extract()[0]
 
-            request = Request("https://leetcode.com" + qUrl, 
+            request = Request("https://leetcode.com" + qUrl,
                             callback=self.parse_item)
             request.meta['qIndex'] = int(qIndex)
             yield request
@@ -41,11 +44,11 @@ class LeetcodeSpider(CrawlSpider):
         item['index'] = str(response.meta['qIndex'])
         item['link'] = response.url
         item['content'] = response.xpath('//div[@class="question-content"]/*').extract()
-        
+
         # there are somecases that page don't load when not logged in
         # omit these pages for now
         if not item['content']:
             return
         item['title'] = response.xpath('//div[@class="question-title"]/h3/text()').extract()[0]
-        
+
         yield item
